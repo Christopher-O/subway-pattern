a34173ac5a451f4dd76062b379260f503e55c2ce
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-05-06 11:43:52 -0600

    Update subway2.js
    updated component name.

--- subway2.js
+++ subway2.js
@@ -89,7 +89,7 @@ var $document = wb.doc,
 			$h1.addClass( toggleClass );
 			$h1Copy.prependTo( $main );
 			$h2.prependTo( $menu );
-			$('.gc-subway-support').appendTo(".gc-subway");
+			$('.gc-subway-support').appendTo(".gc-subway-2");
 		} else if ( ( $html.hasClass( views.sm ) || $html.hasClass( views.xs ) || $html.hasClass( views.xxs ) ) && desktopInited ) {
 
 			// Mobile view, mutate back to mobile first if needed

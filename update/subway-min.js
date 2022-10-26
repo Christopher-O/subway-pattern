5e4ccfd5a3187cde1cf2a19d89797d782ccf0491
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-10-26 11:21:30 -0600

    Update subway-min.js

--- update/subway-min.js
+++ update/subway-min.js
@@ -19,7 +19,7 @@
 		x = function (e) {
 			t = a("<h2 class='h3 hidden-xs visible-md visible-lg mrgn-tp-0'>Sections</h2>"), i = a("<div class='gc-subway-h1' aria-hidden='true'>" + n.text() + "</div>"), a("ul", e).first().wrap("<div class='gc-subway-menu-nav'></div>"), o = a(".gc-subway-menu-nav", e), e.nextUntil(".pagedetails, .gc-subway-section-end").wrapAll("<section class='provisional gc-subway-section'>"), s = e.next(), e.addClass("no-blink"), v = !0
 		};
-	l.on(r.resizeEvents, y), l.on("timerpoke.wb wb-init .gc-subway", d + ".provisional", function (e) {
+	l.on(r.resizeEvents, y), l.on("timerpoke.wb wb-init .gc-subway2", d + ".provisional", function (e) {
 		var t = r.init(e, c, d);
 		t && e.currentTarget === e.target && (e = a(t), (t = (n = a("h1", e)).get(0)) && (t.id = t.id || r.getId(), r.addSkipLink(r.i18n("skip-prefix") + " " + t.textContent, {
 			href: "#" + t.id

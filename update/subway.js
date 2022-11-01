a24043a54c0a21f79a3bc2ba5aed3961c36e2f27
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-11-01 10:01:06 -0600

    Update subway.js
    added index class

--- update/subway.js
+++ update/subway.js
@@ -77,7 +77,7 @@ var $document = wb.doc,
 		// Ensure the page contains at least two heading level 1
 		if ( $( "main h1" ).length < 2 ) {
 			$document.off( wb.resizeEvents, onResize );
-			$elm.addClass( "no-blink p-0" );
+			$elm.addClass( "no-blink p-0 gc-subway-index" );
 			return;
 		}
 
